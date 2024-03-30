85f214c20b7e920042b545620a6503905883300f
Shrutika Jha <shrutikajha2603@gmail.com>
2024-03-30 22:02:56 +0530

    newssss

---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+import Newsitem from "./Newsitem";
+
+const NewsBoard = ({category}) => {
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect (() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=8a198b9ac15c44008476185c87788c9f`;
+    fetch(url).then(response => response.json()).then(data=> setArticles(data.articles));
+  },[category])
+
+  return (
+    <div>
+      <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+      {articles.map((news,index)=>{
+        return <Newsitem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+      })}
+    </div>
+  )
+}
+
+export default NewsBoard
