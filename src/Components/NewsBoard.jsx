13dc8a266833d0c4d37f448d88a5ce01a38283c9
Shrutika Jha <shrutikajha2603@gmail.com>
2024-04-16 15:51:11 +0000

    finall

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -27,7 +27,7 @@ const NewsBoard = ({ category }) => {
 
   return (
     <div>
-      <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+      <h2 className="text-center py-3">Latest <span className="badge bg-danger">News</span></h2>
       {articles && articles.map((news, index) => {
         return <Newsitem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />;
       })}
