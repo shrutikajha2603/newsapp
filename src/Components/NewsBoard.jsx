c81c60029d6fa6e4a05ac157db6dddb1952eb645
Shrutika Jha <shrutikajha2603@gmail.com>
2024-04-16 15:28:08 +0000

    newapi

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,24 +1,41 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import Newsitem from "./Newsitem";
 
-const NewsBoard = ({category}) => {
-
+const NewsBoard = ({ category }) => {
   const [articles, setArticles] = useState([]);
 
-  useEffect (() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=8a198b9ac15c44008476185c87788c9f`;
-    fetch(url).then(response => response.json()).then(data=> setArticles(data.articles));
-  },[category])
+  useEffect(() => {
+    const fetchNews = async () => {
+      const url = `https://news-api14.p.rapidapi.com/top-headlines?country=in&language=en&category=${category}`;
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '92e0777d37msh14265b5cf2e6ca2p1048d8jsn3a3799025e24',
+          'X-RapidAPI-Host': 'news-api14.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+        setArticles(result.articles);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchNews();
+  }, [category]);
 
   return (
     <div>
       <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-      {articles && articles.map((news,index) => {
-        return <Newsitem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+      {articles && articles.map((news, index) => {
+        return <Newsitem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />;
       })}
     </div>
   );
-  
-}
+};
 
-export default NewsBoard
+export default NewsBoard;
