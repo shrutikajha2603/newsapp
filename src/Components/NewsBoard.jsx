d2e3d37cf63fe729f884cfe81a259a0caa31a46b
Shrutika Jha <shrutikajha2603@gmail.com>
2024-04-16 15:43:28 +0000

    api fixed final

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -6,13 +6,10 @@ const NewsBoard = ({ category }) => {
 
   useEffect(() => {
     const fetchNews = async () => {
-      const url = `https://news-api14.p.rapidapi.com/top-headlines?country=in&language=en&category=${category}`;
+      const url = `https://saurav.tech/NewsAPI/top-headlines/category/${category}/in.json`;
       const options = {
         method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': '92e0777d37msh14265b5cf2e6ca2p1048d8jsn3a3799025e24',
-          'X-RapidAPI-Host': 'news-api14.p.rapidapi.com'
-        }
+        
       };
 
       try {
